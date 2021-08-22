b474f5e7a2c0e0cc0764c1db4acebca3a73481e4
Imran Hossain <imranmihab@gmail.com>
2021-08-22 22:53:42 +0600

    Initial Commit

---
+++ app/src/main/java/com/mihab/roomwordapp/WordDao.kt
@@ -0,0 +1,20 @@
+package com.mihab.roomwordapp
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface WordDao {
+
+    @Query("SELECT * FROM word_table ORDER BY word ASC")
+    fun getAlphabetizeWords(): Flow<List<Word>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(word: Word)
+
+    @Query("DELETE FROM word_table")
+    suspend fun deleteAll()
+}
